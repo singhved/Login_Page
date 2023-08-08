5633dd6d7aa79e99c9c9f40294e9c3e2cff2cbbf
singhved <vedprakashs039@gmail.com>
2023-08-08 17:13:21 +0530

    New Model Added

---
+++ Models/CustomAuthFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+using System.Web.Routing;
+
+namespace CustomAuthenticationFilter
+{
+    public class CustomAuthFilter : ActionFilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(filterContext.HttpContext.Session["UserName"])))
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result == null || filterContext.Result is HttpUnauthorizedResult)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary
+                {
+                     { "controller", "Account" },
+                     { "action", "Login" }
+                });
+            }
+        }
+    }
+}
